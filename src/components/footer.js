4b6ba817dd03afd658914754529cc3920f416e82
ankitvat <ankitvat@users.noreply.github.com>
2021-01-13 16:41:53 +0530

    added footer logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,34 +5,48 @@ import { AiOutlineLinkedin } from '@react-icons/all-files/ai/AiOutlineLinkedin'
 import { AiOutlineFacebook } from '@react-icons/all-files/ai/AiOutlineFacebook'
 import { AiOutlineInstagram } from '@react-icons/all-files/ai/AiOutlineInstagram'
 import { AiOutlineTwitter } from '@react-icons/all-files/ai/AiOutlineTwitter'
-
+import { AiOutlineCopyright } from '@react-icons/all-files/ai/AiOutlineCopyright'
+import logo from '../images/logo.png'
+import oldlogo from '../images/oldlogo.png'
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="footer-links">
-        <Link to="#">Our Journey</Link>
-        <Link to="#">Products</Link>
-        <Link to="#">Stories</Link>
-        <Link to="#">Contact</Link>
-      </div>
-      <div className="footer-links">
-        <Link to="#">Terms of Use</Link>
-        <Link to="#">FAQ</Link>
-        <Link to="#">Privacy Policy</Link>
+      <div className="left-footer">sadad</div>
+      <div className="right-footer">
+        <div className="footer-links">
+          <h1 className="footer-heading">Navigation</h1>
+          <Link to="#">Our Journey</Link>
+          <Link to="#">Products</Link>
+          <Link to="#">Stories</Link>
+        </div>
+        <div className="footer-links">
+          <h1 className="footer-heading">Company</h1>
+          <Link to="#">Terms Of Use</Link>
+          <Link to="#">FAQ</Link>
+          <Link to="#">Privacy Policy</Link>
+        </div>
       </div>
       <div className="socials">
-        <Link to="#">
-          <AiOutlineFacebook />
-        </Link>
-        <Link to="#">
-          <AiOutlineInstagram />
-        </Link>
-        <Link to="#">
-          <AiOutlineLinkedin />
-        </Link>
-        <Link to="#">
-          <AiOutlineTwitter />
-        </Link>
+        <div className="social-heading">
+          <h1>Follow us </h1>
+        </div>
+        <div className="social-links">
+          <Link to="#">
+            <AiOutlineFacebook />
+          </Link>
+          <Link to="#">
+            <AiOutlineInstagram />
+          </Link>
+          <Link to="#">
+            <AiOutlineLinkedin />
+          </Link>
+          <Link to="#">
+            <AiOutlineTwitter />
+          </Link>
+          <div className="social-footer">
+            <AiOutlineCopyright className="icon" /> <h1>2021 HonestFarmer</h1>
+          </div>
+        </div>
       </div>
     </div>
   )
