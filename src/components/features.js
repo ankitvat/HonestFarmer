883c2f62ed42f50712ae43c24dd6d6e9be44860a
ankitvat <ankitvat@users.noreply.github.com>
2021-01-13 12:14:08 +0530

    Updated Features

--- src/components/features.js
+++ src/components/features.js
@@ -3,8 +3,8 @@ import '../styles/features.scss'
 
 const texts = [
   ' Our produce is grown from the best varieties of seed, and irrigated with clean ground water. We work with farmers who follow cutting-edge farming techniques. The best of thier harvest is delivered to you, free from any pesticide residue.',
-  'Quisque iaculis, odio quis lacinia vulputate, ligula metus vehicula arcu, sit amet iaculis enim leo in ligula. Vivamus condimentum finibus tortor, vel dapibus ex ultrices sit amet. Cras non mauris sed nisi viverra imperdiet. Ut aliquam tincidunt felis ut dictum. Curabitur ut tempus nulla. Proin at eleifend neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus tristique, enim et consectetur sodales, felis massa ultricies lectus, quis porta nunc enim non libero. ',
-  'Sed pellentesque dolor at consequat lobortis. Phasellus sollicitudin congue dui sit amet mollis. Quisque non lacus eu metus porta bibendum vitae et tellus. Proin ultricies odio lectus, id pulvinar neque volutpat id. Pellentesque vulputate lacinia elit, quis tincidunt turpis auctor at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean vel orci sed purus rutrum tincidunt in vitae nisi. Duis dapibus aliquam sem, quis maximus orci faucibus eget. ',
+  'A QR code on the packaging tells you where your product came from, and how long it took to reach you.',
+  'All items with a short shelf life are procured after we receive orders. Because we take the guesswork out of consumer demand, we never end up with more than we can sell. The result? Zero waste.',
 ]
 
 const classes = ['green', 'blue', 'red']
@@ -23,21 +23,23 @@ export default function Test() {
               onMouseEnter={() => setHovered(0)}
               onMouseLeave={() => setHovered(-1)}
             >
-              Fresh apples, from the land of Karan Chauhan
+              <p>Progressive Farmers.</p>
             </div>
             <div
+              className="hovertext"
               role="presentation"
               onMouseEnter={() => setHovered(1)}
               onMouseLeave={() => setHovered(-1)}
             >
-              Hassle free. Free Delivery at your door step
+              Tracable Food.
             </div>
             <div
+              className="hovertext"
               role="presentation"
               onMouseEnter={() => setHovered(2)}
               onMouseLeave={() => setHovered(-1)}
             >
-              Pesticides and chemicals free
+              Zero Waste
             </div>
           </div>
         </div>
@@ -50,7 +52,7 @@ export default function Test() {
                 </h1>
               </div>
             ) : (
-              <p className="image-text">{texts[hovered]}</p>
+              <p className="text">{texts[hovered]}</p>
             )}
           </div>
         </div>
